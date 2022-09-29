6f2c76166a2d507a19f622036d35b9fc69995317
yemufei <yemufei@126.com>
2022-09-29 16:19:29 +0800

    a

--- SpringBoot/spring-boot01-helloworld/src/main/java/com/wdjr/controller/HelloController.java
+++ SpringBoot/spring-boot01-helloworld/src/main/java/com/wdjr/controller/HelloController.java
@@ -1,15 +1,21 @@
 package com.wdjr.controller;
 
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloController {
+public class HelloController implements BeanNameAware {
 
     @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("setBeanName invoke");
     }
 }
